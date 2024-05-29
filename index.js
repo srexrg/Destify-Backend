bae6cca1ea6453635ac5dcb575475862ecfeffd6
srexrg <sreeragp276@gmail.com>
2024-05-29 23:30:51 +0530

    refactor: Improve formatting and readability of travel prompt message

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 import "dotenv/config";
 
 async function fetchData(preferences, budget, numTravelers, month,currency) {
-  const prompt = `Discover your ideal travel destination based on your preferences. Plan your trip with a budget of ${currency}${budget} for  ${numTravelers} travelers, intending to travel in the month of ${month}.Your main preferences are ${preferences}.Immerse yourself in breathtaking scenic landscapes, embrace rich cultural experiences, and explore thrilling adventure opportunities.
-      Additionally, gain insights into the local community and their way of life. 
-      Please avoid using bold text to highlight the responses.
-      only use commonmark markdown for response, use #, ##, ### for headings.`;
+  const prompt = `Discover your ideal travel destination based on your preferences. Plan your trip with a budget of ${currency} ${budget} for ${numTravelers} travelers, intending to travel in the month of ${month}. Your main preferences are ${preferences}. Immerse yourself in breathtaking scenic landscapes, embrace rich cultural experiences, and explore thrilling adventure opportunities. Additionally, gain insights into the local community and their way of life. Please avoid using bold text to highlight the responses and use CommonMark Markdown for the response, with headings formatted using #, ##, ###.  `;
 
   const genAI = new GoogleGenerativeAI(process.env.API_KEY);
   const model = genAI.getGenerativeModel({ model: "gemini-pro" });
